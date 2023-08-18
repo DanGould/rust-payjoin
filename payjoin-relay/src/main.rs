051c17a13b38e17b69b7828ad8f0be814f59a4ea
DanGould <d@ngould.dev>
2023-08-18 14:19:34 -0400

    Listen for WebTransport with self-signed cert

--- payjoin-relay/src/main.rs
+++ payjoin-relay/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Serverless payjoin relay stub");
+use std::net::{Ipv6Addr, SocketAddr};
+
+use wtransport::tls::Certificate;
+use wtransport::{Endpoint, ServerConfig};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cert = Certificate::load("example_cert/cert.pem", "example_cert/key.pem")?;
+    let config = ServerConfig::builder()
+        .with_bind_address(SocketAddr::new(Ipv6Addr::LOCALHOST.into(), 4433))
+        .with_certificate(cert)
+        .build();
+    let server = Endpoint::server(config)?;
+
+    println!("Serverless payjoin relay awaiting WebTransport connection on port 4433");
+    let incoming_request = server.accept().await.await?;
+    let _connection = incoming_request.accept().await?;
+    Ok(())
 }
