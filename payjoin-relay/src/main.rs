7d46a10c8124dac60b45ae2c1f0bbea7fb1e4f5c
DanGould <d@ngould.dev>
2023-08-18 14:19:34 -0400

    Listen for WebSockets

--- payjoin-relay/src/main.rs
+++ payjoin-relay/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Serverless payjoin relay stub");
+use std::net::{Ipv6Addr, SocketAddr};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let server = TcpListener::bind(SocketAddr::new(Ipv6Addr::LOCALHOST.into(), 8080)).await?;
+
+    println!("Serverless payjoin relay awaiting WebTransport connection on port 4433");
+    let (_stream, _) = server.accept().await?;
+    Ok(())
 }
