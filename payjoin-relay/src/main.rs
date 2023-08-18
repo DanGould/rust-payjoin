bd4fab71147b7cae8073a3cceb4878d28de4c857
DanGould <d@ngould.dev>
2023-08-18 14:19:34 -0400

    Listen for HTTP

--- payjoin-relay/src/main.rs
+++ payjoin-relay/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Serverless payjoin relay stub");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(|_req| async {
+            Ok::<_, Infallible>(Response::new(Body::from("Hello, World!")))
+        }))
+    });
+    println!("Serverless payjoin relay awaiting HTTP connection on port 8080");
+    hyper::Server::bind(&"0.0.0.0:8080".parse()?).serve(make_svc).await?;
+    Ok(())
 }
