6454c269156e3c6b1fc50314085bcbcb2b0d0210
DanGould <d@ngould.dev>
2023-08-18 14:19:34 -0400

    Listen for HTTP

--- payjoin-relay/src/main.rs
+++ payjoin-relay/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Serverless payjoin relay stub");
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse()?).serve(app.into_make_service()).await?;
+    println!("Serverless payjoin relay awaiting HTTP connection on port 8080");
+    Ok(())
 }
