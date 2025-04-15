a5cef46a6c18f6ceeac941fe18b723faede65b6d
DanGould <d@ngould.dev>
2025-04-15 15:17:35 -0400

    Take url from public API as IntoUrl everywhere
    We missed a few on the last pass. impl IntoUrl is more ergonomic.

--- payjoin/src/send/multiparty/mod.rs
+++ payjoin/src/send/multiparty/mod.rs
@@ -16,7 +16,7 @@ use crate::output_substitution::OutputSubstitution;
 use crate::persist::{self, Persister};
 use crate::send::v2::{ImplementationError, V2PostContext};
 use crate::uri::UrlExt;
-use crate::{PjUri, Request};
+use crate::{IntoUrl, PjUri, Request};
 
 mod error;
 
@@ -79,7 +79,7 @@ impl Sender {
     }
     pub fn extract_v2(
         &self,
-        ohttp_relay: Url,
+        ohttp_relay: impl IntoUrl,
     ) -> Result<(Request, PostContext), CreateRequestError> {
         let rs = self
             .0
@@ -158,7 +158,7 @@ impl GetContext {
     /// Extract the GET request that will give us the psbt to be finalized
     pub fn extract_req(
         &self,
-        ohttp_relay: Url,
+        ohttp_relay: impl IntoUrl,
     ) -> Result<(Request, ohttp::ClientResponse), crate::send::v2::CreateRequestError> {
         self.0.extract_req(ohttp_relay)
     }
@@ -216,7 +216,7 @@ pub struct FinalizeContext {
 impl FinalizeContext {
     pub fn extract_req(
         &self,
-        ohttp_relay: Url,
+        ohttp_relay: impl IntoUrl,
     ) -> Result<(Request, ohttp::ClientResponse), CreateRequestError> {
         let reply_key = self.hpke_ctx.reply_pair.secret_key();
         let body = serialize_v2_body(
